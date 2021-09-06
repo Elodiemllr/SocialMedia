603b38277fbef5b7b6900b26ce8dd79f46d92264
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-06 14:16:14 +0200

    creation of post action and post reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,10 +1,12 @@
 //regroupe tous les reducer
 
 import { combineReducers } from "redux";
+import postReducer from "./post.reducer.js";
 import userReducer from "./user.reducer.js";
 import usersReducer from "./users.reducer.js";
 
 export default combineReducers({
     userReducer,
     usersReducer,
+    postReducer,
 });
