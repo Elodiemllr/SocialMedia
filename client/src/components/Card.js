46bff13d37c58731ea2ba997246a3ed00b75517e
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-06 16:25:33 +0200

    card with loading

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 //comp individuel pour un post (mit dans le thread)
-const Card = () => {
-    return <div> card</div>;
+
+//on se recupère les données dans "posts"
+const Card = ({ post }) => {
+    //tant qu'on a pas la data de notre card, on met un "loading"
+    const [isLoading, setIsLoading] = useState(true);
+
+    return (
+        <li className="card-container" key={post._id}>
+            {/*si "isLoading" est sur true, alors on met la classe fas fa-spinner */}
+            {isLoading ? (
+                <i className="fas fa-spinner fa-spin"></i>
+            ) : (
+                //sinon on met ça
+                <h2> ttest</h2>
+            )}
+        </li>
+    );
 };
 
 export default Card;
