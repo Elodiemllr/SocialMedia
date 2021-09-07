ba819885336b9cf72401a865286826a324103f63
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-07 18:37:29 +0200

    creation of NewPostForm

---
+++ client/src/components/Post/NewPostForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPostForm = () => {
+    <div> new post </div>;
+};
+
+export default NewPostForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UidContext } from "../components/AppContext.js";
 import LeftNavBar from "../components/LetNavBar.js";
+import Log from "../components/log";
+import NewPostForm from "../components/Post/NewPostForm.js";
 import Thread from "../components/Thread.js";
 //ici je renvoie tout ce qu'il concerne l'accueil alors que dans components je fais des comp individuel
 
 const Home = () => {
+    //on verifie si notre user est connecté
+    const uid = useContext(UidContext);
     return (
         <div className="home">
             {/* on importe notre leftBar */}
             <LeftNavBar />
             <div className="main">
+                {/*l'affichage change en fonction de si l'user est connecté ou pas */}
+                <div className="home-header">
+                    {/*si on a uid, que notre user est présent alors on affiche le formaulaire pour poster sinon on affiche pour se loguer*/}
+
+                    {uid ? (
+                        <NewPostForm />
+                    ) : (
+                        <Log signin={true} signup={false} />
+                    )}
+                </div>
                 <Thread />
             </div>
         </div>
