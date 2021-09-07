883b5efc4ae54440817a1c6f7aa5d5138f082747
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-07 19:44:00 +0200

    update newpostform with preview message, profil link, video and pic

--- client/src/components/Post/NewPostForm.js
+++ client/src/components/Post/NewPostForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { isEmpty } from "../../Utils.js";
-
+import { NavLink } from "react-router-dom";
+import { isEmpty, timestampParser } from "../../Utils.js";
 const NewPostForm = () => {
     //pour le chargement de la page
     const [isLoading, setIsLoading] = useState(true);
@@ -11,11 +11,42 @@ const NewPostForm = () => {
     const [file, setFile] = useState("");
     const userData = useSelector((state) => state.userReducer);
 
+    const handlePicture = () => {};
+
+    const handlePost = () => {};
+
+    const cancelPost = () => {
+        //on passe tout sur des string vide / on annule tout
+        setMessage("");
+        setPostPicture("");
+        setVideo("");
+        setFile("");
+    };
+
+    const handleVideo = () => {
+        //on se split tout notre message partie par partie;
+        let findLink = message.split(" ");
+        //on test chaque élement splité
+        for (let i = 0; i < findLink.length; i++) {
+            //si ça inclus 'https......' ou
+            if (
+                findLink[i].includes("https://www.yout") ||
+                findLink[i].includes("https://yout")
+            ) {
+                // on remplace replace('watch?v=' par  "embed/"
+                let embed = findLink[i].replace("watch?v=", "embed/");
+                //on coupe la partie de notre link après "&" pour ne pas poster une vidéo youtube dejà avancé dans le temps
+                setVideo(embed.split("&")[0]);
+            }
+        }
+    };
+
     //on verifie que notre userData ne soit pas vide, si il ne l'est pas alors on passe notre loading sur false
     useEffect(() => {
         if (!isEmpty(userData)) setIsLoading(false);
-        //on relance notre useEffect dès que la data evolue
-    }, [userData]);
+        handleVideo();
+        //on relance notre useEffect dès que la data evolue ou message evolu
+    }, [userData, message, video]);
 
     return (
         <div className="post-container">
@@ -53,6 +84,97 @@ const NewPostForm = () => {
                                 ? "s"
                                 : ""}
                         </p>
+                    </div>
+                    {/*au click sur la photo de l'user on atterie sur son profil*/}
+                    <NavLink exact to="/profil">
+                        <div className="user-info">
+                            <img src={userData.picture} alt="user-img"></img>
+                        </div>
+                    </NavLink>
+                    {/*le form*/}
+                    <div className="post-form">
+                        <textarea
+                            name="message"
+                            id="message"
+                            placeholder="Ecrire.."
+                            onChange={(e) => setMessage(e.target.value)}
+                            value={message}
+                        ></textarea>
+                        {/* Si message il est sur 'true' on affiche un previex*/}
+                        {message || postPicture || video.length > 20 ? (
+                            <li className="card-container">
+                                <div className="card-left">
+                                    <img
+                                        src={userData.picture}
+                                        alt="user-pic"
+                                    ></img>
+                                </div>
+                                <div className="card-right">
+                                    <div className="card-header">
+                                        <div className="pseudo">
+                                            <h3>{userData.pseudo}</h3>
+                                        </div>
+                                        {/* nous permet d'afficher la date en temps reel*/}
+                                        <span>
+                                            {timestampParser(Date.now())}
+                                        </span>
+                                    </div>
+                                    <div className="content">
+                                        <p> {message}</p>
+                                        <img src={postPicture} alt="" />
+                                        {video && (
+                                            <iframe
+                                                src={video}
+                                                frameBorder="0"
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                allowFullScreen
+                                                title={video}
+                                            ></iframe>
+                                        )}
+                                    </div>
+                                </div>
+                            </li>
+                        ) : null}
+                        {/*on fait une condition, si on met une photo on ne pourra pas mettre de vidéo et vice versa*/}
+                        <div className="footer-form">
+                            <div className="icon">
+                                {isEmpty(video) && (
+                                    <>
+                                        <img
+                                            src="./img/icons/picture.svg"
+                                            alt="img"
+                                        />
+                                        <input
+                                            type="file"
+                                            id="file-upload"
+                                            name="file"
+                                            accept="jpg, jpeg, png"
+                                            onChange={(e) => handlePicture(e)}
+                                        />
+                                    </>
+                                )}
+                                {video && (
+                                    <button onClick={() => setVideo("")}>
+                                        {" "}
+                                        Supprimer Video{" "}
+                                    </button>
+                                )}
+                            </div>
+                            <div className="btn-send">
+                                {/*cancel post ne s'affichera que si il y'a quelques chose a effacer*/}
+                                {message || postPicture || video.length > 20 ? (
+                                    <button
+                                        className="cancel"
+                                        onClick={cancelPost}
+                                    >
+                                        Annuler le message{" "}
+                                    </button>
+                                ) : null}
+                                <button className="send" onClick={handlePost}>
+                                    Envoyer
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </>
             )}
