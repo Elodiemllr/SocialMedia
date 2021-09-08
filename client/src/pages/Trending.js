7a788f6809fae7e1bb43201c6c2db4144737baed
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-08 19:26:46 +0200

    creation of trending page

--- client/src/pages/Trending.js
+++ client/src/pages/Trending.js
@@ -1,9 +1,27 @@
-import React from "react";
-
-//ici je renvoie tout ce qu'il concerne les tendances alors que dans components je fais des comp individuel
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import { UidContext } from "../components/AppContext";
+import LeftNavBar from "../components/LetNavBar.js";
+import Card from "../components/Post/Card";
+import { isEmpty } from "../Utils.js";
 
 const Trending = () => {
-    return <div className="trending-page">trending</div>;
+    const uid = useContext(UidContext);
+    const trendList = useSelector((state) => state.trendingReducer);
+
+    return (
+        <div className="trending-page">
+            <LeftNavBar />
+            <div className="main">
+                <ul>
+                    {!isEmpty(trendList[0]) &&
+                        trendList.map((post) => (
+                            <Card post={post} key={post._id} />
+                        ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Trending;
