9338fc8b0111228f3619424037e64e92ba67af5a
elodiemllr <elodie.muller.pro@outlook.com>
2021-09-12 15:07:31 +0200

    save state in store

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
 body {{background: ${({ theme }) => theme.body};
 color:  ${({ theme }) => theme.text};
+transition: all .5s linear;
 }
 .post-container {
     background ${({ theme }) => theme.primary};
